e3ff968ae3e74b9da38ef70d1ab8409f81b7a65d
billhowland <electronzero@comcast.net>
2019-02-05 19:28:27 -0800

    changed & new

---
+++ python/LabOpt-SockSorter2.py
@@ -0,0 +1,41 @@
+# SockSorter.py
+# Version 2:
+
+import random
+allsocks = []
+socktypes = ['ankle', 'crew', 'calf', 'thigh']
+colors = ['black', 'white', 'blue']
+sorter = {}
+
+#define functions here-------------------------------------------------------------------------------------------------------
+
+# def
+
+#---------------------------------------------------------------------------------------------------------------------------
+
+run = 1
+while run:
+
+#---|TAB---------------------------------------------------------------------------------------------------------------------
+
+    for i in range(100):
+        sock = random.choice(socktypes)
+        color = random.choice(colors)
+        allsocks.append((sock, color))
+
+        sorter[(sock, color)] = sorter.get((sock, color), 0) + 1
+
+
+    print(allsocks)
+    print(sorter)
+
+    for sock in sorter:
+        print(f'{sock} has {sorter[sock]%2} loner(s)')
+
+
+
+    #----------------------------------------------------------------------------------------------------------------------------
+
+    ask = input('Quit? Y/N > ').strip().lower()
+    if ask == 'y':
+        run = 0
