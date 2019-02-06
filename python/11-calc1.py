546191a7fe329a94b985422b867b59427d751204
billhowland <electronzero@comcast.net>
2019-02-05 20:15:42 -0800

    new & improved

--- python/11-calc1.py
+++ python/11-calc1.py
@@ -1,5 +1,5 @@
-# 99-template.py
-# Version X:
+# 11-calc1.py
+# Version 1:
 
 #define functions here-------------------------------------------------------------------------------------------------------
 
@@ -12,6 +12,24 @@ while run:
 
 #---|TAB---------------------------------------------------------------------------------------------------------------------
 
+    op = input('What is the operation? ')
+    numa = float(input('what is the first number? '))
+    numb = float(input('what is the second number? '))
+
+    print((((str(numa))) + ' ' + (op) + ' ' + (str((numb))) + ' = '), end = '')
+    if op == '+':
+        print (numa + numb)
+    elif op == '-':
+        print (numa - numb)
+    elif op == '/':
+        if numb != 0:
+            print (numa / numb)
+        else:
+            print ("I don't divide by 0, idiot!")
+    elif op == '*':
+        print (numa * numb)
+    else:
+        print("bogus operator, retry")
 
 
 #----------------------------------------------------------------------------------------------------------------------------
