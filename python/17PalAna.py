b7280ae60fc485ff084ac31b9c84c867d40ee545
billhowland <electronzero@comcast.net>
2019-02-16 09:03:16 -0800

    Updated

--- python/17PalAna.py
+++ python/17PalAna.py
@@ -5,14 +5,31 @@
 
 
 def check_palindrome(word):
-    pass
+    if word == word[::-1]:
+        return (f'Yes, {word} is a palindrome.')
+    else:
+        return (f'No, {word} is not a palindrome.')
 
 
-# def func():
+def check_anagram(worda, wordb):
+    # Remove all spaces from each word (replace) str.replace(old, new[, max])
+    # worda = worda.replace(' ', '')
+    # wordb = wordb.replace(' ', '')
+    # Sort the letters of each word (sorted) b = sorted(a) returns a list
+    # sworda = sorted(worda)
+    # swordb = sorted(wordb)
+    # Check if the two are equal
+    # return sworda == swordb
+    return (sorted(worda.replace(' ', '')) == sorted(wordb.replace(' ', '')))
+
 
 def main():
-    word = input('enter a word: ')
-    check_palindrome(word)
+    word = input('Enter a word: ').strip().lower()
+    print(check_palindrome(word))
+
+    worda = input('Enter the first word: ').strip().lower()
+    wordb = input('Enter the second word: ').strip().lower()
+    print(check_anagram(worda, wordb))
 
     # ----------------------------------------------------------------
 
