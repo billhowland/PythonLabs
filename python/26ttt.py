2daf8788812855b4ebf8e7f503fa8e3dbeb4f0fa
billhowland <electronzero@comcast.net>
2019-03-01 21:13:01 -0800

    updated slowly

--- python/26ttt.py
+++ python/26ttt.py
@@ -1,25 +1,72 @@
-# 99-template.py
-# Version X:
+# 26ttt.py
+# Version 1: Tic Tac Toe
 
-# define functions here-------------------------------------------
 
-# def func():
+class Player:
+    def __init__(self, name, token):
+        self.name = name
+        self.token = token
 
-# def func():
 
-# def main():
+class Game:
+    def __init__(self):
+        self.board = [['1', '2', '3'],
+                      ['4', '5', '6'],
+                      ['7', '8', '9']]
+
+    def __repr__(self):
+        board = ''
+        for row in self.board:
+            board += '|'.join(row) + '\n'
+        return board
+
+    def move(self, player, x, y):
+        # figure out x, y
+        if self.board[y][x] not in ['a', 'b']:
+            self.board[y][x] = player
+            return True
+        else:
+            return False
+
+
+# ---------------------------------------------------------------
+
+def spot_number(board, player):
+    while True:
+        try:
+            # a = int(input())
+            move = board.move(player, 0, 0)
+            if move:
+                break
+            else:
+                print("\nSpace Occupied, try again")
+                break
+        except ValueError:
+            print("\nInvalid, try again")
+
+
+def main():
+    board = Game()
+    players = []
+    # board.board[1][1] = 'x'
+    players.append(input("What is player one's name? > "))
+    players.append(input("What is player two's name? > "))
+    print("Great! Let's play some Tic Tac Toe!")
+    for player in players:
+        print(f'{player}, please pick a spot')
+        spot_number(board, player)  # aint gonna work
+        print(board)
+
 
 # ----------------------------------------------------------------
+
+
 if __name__ == '__main__':
 
     run = 1
     while run:
 
-        # --------------------------------------------------------
-
         main()
-
-# ----------------------------------------------------------------
 
         ask = input('Quit? Y/N > ').strip().lower()
         if ask == 'y':
