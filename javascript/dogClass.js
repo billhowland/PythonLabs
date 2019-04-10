4035f6840ada3c269380f90238574dfdab9a96b2
billhowland <electronzero@comcast.net>
2019-04-09 20:07:59 -0700

    changes

---
+++ javascript/dogClass.js
@@ -0,0 +1,22 @@
+// dogClass.js
+
+// class Dog:
+//     def __init__(self, name, size, age):
+//         self.name = name
+//         self.size = size
+//         self.age = age
+//
+//     def bark(self):
+//         print(f'my name is {self.name}, I am a {self.size} dog that is {self.age} years old')
+
+class Dog {
+  constructor(name, breed) {
+    this.name = name
+    this.breed = breed
+    this.age = age
+  }
+
+  bark() {
+    console.log('bark! my name is ${this.name}. I am a ${this.breed} that is ${this.age} years old')
+
+  }
