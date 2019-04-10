dd938f9e8cad628a45189ac1dff82be4cd46402a
billhowland <electronzero@comcast.net>
2019-04-09 21:29:46 -0700

    updates

--- javascript/07-hackertyper/JS/07-hackertyper.js
+++ javascript/07-hackertyper/JS/07-hackertyper.js
@@ -1,5 +1,5 @@
 const consoleDiv = document.querySelector('#console')
-const filepath = '/media/bill/ServalData/PDX-Code-Guild/Jan-2019-Night/javascript/07-hackertyper/uchess2.txt'
+const filepath = '/javascript/07-hackertyper/uchess2.txt'
 let text
 const getText = async () => {
   const res = await fetch(filepath)
