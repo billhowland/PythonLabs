8364eb14740e9ad4d1b423f7ea09743e15797dc4
billhowland <electronzero@comcast.net>
2019-04-19 21:26:15 -0700

    updates

--- django/blog/views.py
+++ django/blog/views.py
@@ -2,6 +2,8 @@
 from .models import Post
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
+
 
 # Create your views here.
 
@@ -14,3 +16,8 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
