7ce1a574f92ea772776f735532df8ab9be914405
billhowland <electronzero@comcast.net>
2019-04-22 20:59:45 -0700

    updates

--- django_todo/django_todo/todo_list/models.py
+++ django_todo/django_todo/todo_list/models.py
@@ -1,3 +1,16 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Task(models.Model):
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    completed_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.text
