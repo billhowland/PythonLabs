56a38925cac62807db75643b482ff9117b35c0da
billhowland <electronzero@comcast.net>
2019-04-30 18:27:43 -0700

    updates

--- URLS/urlslab/views.py
+++ URLS/urlslab/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from .models import Task
+from .forms import TaskForm
 
 # Create your views here.
 
 
 def url_shortener(request):
-    return render(request, 'urlslab/url_shortener.html', {})
+    if POST:
+        url_input = req.POST['url']
+        url = URL(long=url_input,
+                  short=shorten_alg(url_in))
+        url.save()
+        redirect('show_url', {long: long})
+
+
+def redirect(reg, short_url):
+    url = get_object_or_404(URL, short=short_url)
+    redirect(url.long)
+
+
+def index(reg):
