c0e8d37bb239f724d46f7419937004d5caf0dd10
billhowland <electronzero@comcast.net>
2019-05-03 21:08:39 -0700

    redo!

---
+++ Django-URLSqueezer/URLSqueezer/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/media/bill/ServalData/PDX-Code-Guild/Jan-2019-Night/Django-URLSqueezer/URLSqueezer/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
