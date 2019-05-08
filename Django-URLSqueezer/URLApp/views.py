fc50ff7095422813888645319ba4ea1bce946ab3
billhowland <electronzero@comcast.net>
2019-05-07 21:06:26 -0700

    finish url crusher

--- Django-URLSqueezer/URLApp/views.py
+++ Django-URLSqueezer/URLApp/views.py
@@ -3,23 +3,23 @@ from .models import Shrink
 
 
 def show_url(request, short_url):
-    short_url = get_object_or_404(Shrink, short_url=short_url)
-    return render(request, 'URLApp/urls_base.html', {'url': short_url})
+    url = get_object_or_404(Shrink, short_url=short_url)
+    if url.long_url.startswith('http'):
+        return redirect(url.long_url)
+    else:
+        return redirect('http://' + url.long_url)
 
 
 def urls_base(request):
     if request.method == "POST":
-        shrink = Shrink()
-        shrink.long_url = request.POST.get('input_url')
+        url_input = request.POST['input_url']
+        if not Shrink.objects.filter(long_url=url_input).exists():
+            shrink = Shrink(long_url=url_input, short_url=shorten_alg())
+            shrink.save()
+        else:
+            shrink = Shrink.objects.get(long_url=url_input)
+        return render(request, 'URLApp/urls_base.html', {'url': shrink})
 
-        short_temp_url = shorten_alg()
-        shrink.short_url = short_temp_url
-        shrink.save()
-
-        # url_input = request.POST['url']
-        # url_input = Shrink(long_url=url_input, short_url=shorten_alg(url_input))
-
-        return redirect('show_url', shrink.short_url)
         # return render(request, 'URLApp/url_output.html', {"short_url": shrink.short_url})
     return render(request, 'URLApp/urls_base.html')
 
